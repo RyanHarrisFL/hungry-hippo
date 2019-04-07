a95511e4865d5f4dc3d94e2ecbbcb04bc65fd16f
Rhino <ryanharrisfl@gmail.com>
2019-04-07 14:40:21 -0400

    added button for eating food

--- public/assets/js/hippos.js
+++ public/assets/js/hippos.js
@@ -1,1 +1,47 @@
-console.log("This is temporary!");
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newFood = $(this).data("devoured");
+  
+      var newHungerState = {
+        devoured: newFood
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/cats/" + id, {
+        type: "PUT",
+        data: newHungerState
+      }).then(
+        function() {
+          console.log("changed sleep to", newFood);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  /*
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newCat = {
+        name: $("#ca").val().trim(),
+        sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });  
+*/
+  });
+  
