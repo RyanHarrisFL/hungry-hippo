433580cdb501d8aff8070402fc6a732603f0ce5e
Rhino <ryanharrisfl@gmail.com>
2019-04-08 16:05:48 -0400

    change to sql inserts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
+USE hippos_db;
+
 INSERT INTO hippos (food_name)
 VALUES ("lions");
 
-INSERT INTO hippos (name, devoured)
+INSERT INTO hippos (food_name, devoured)
 VALUES ("marbles", true);
 
 INSERT INTO hippos (food_name, devoured)
